ddf6975e2e5c96cc681e4d284db603f71dfa42ea
Sanjay <sanjayma@gmail.com>
2026-07-31 22:38:44 -0700

    Fix: CFBundleTypeName in CFBundleDocumentTypes

--- Tests/DocumentTypesTests.swift
+++ Tests/DocumentTypesTests.swift
@@ -16,21 +16,25 @@ final class DocumentTypesTests: XCTestCase {
 
         XCTAssertEqual(documentTypes.count, 2)
         assertDocumentType(
-            documentTypes[0], contentType: "public.text",
-            role: "Editor", rank: "Default")
+            documentTypes[0], name: "Text Document",
+            contentType: "public.text", role: "Editor", rank: "Default")
         assertDocumentType(
-            documentTypes[1], contentType: "public.data",
-            role: "Viewer", rank: "Alternate")
+            documentTypes[1], name: "Data File",
+            contentType: "public.data", role: "Viewer", rank: "Alternate")
     }
 
     private func assertDocumentType(
         _ documentType: [String: Any],
+        name: String,
         contentType: String,
         role: String,
         rank: String,
         file: StaticString = #filePath,
         line: UInt = #line
     ) {
+        XCTAssertEqual(
+            documentType["CFBundleTypeName"] as? String,
+            name, file: file, line: line)
         XCTAssertEqual(
             documentType["LSItemContentTypes"] as? [String],
             [contentType], file: file, line: line)
